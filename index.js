203a381f0a397f624d696097ad9e62b1d7a8cf52
Rkebede <rutht.kebede@gmail.com>
2021-02-18 14:05:29 -0600

    finding the currentSum and largest sum

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
 function largestSubarraySum(array) {
-
-  return array.reduce((num, acc) => num + acc)
-
+  let currentSum = 0
+  let largestSum = 0
   
-  
+  for(let num of array){
+    currentSum = Math.max(0, (currentSum + num))
+    largestSum = Math.max(largestSum, currentSum)
+  }
+
+  return largestSum
+
 }
 
